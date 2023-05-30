1c38451921c985ba07a42f496f6c4646bd628f3f
tmaxadmin <Admin@tmaxadmin>
2023-05-30 14:31:39 +0900

    Table top Search ToggleButton 적용

--- src/components/organisms/CmCommonTable/filterControls/FilterControls.tsx
+++ src/components/organisms/CmCommonTable/filterControls/FilterControls.tsx
@@ -1,12 +1,74 @@
-import { Button, Stack } from '@mui/material';
 import React, { useCallback, useState } from 'react';
+
+import ArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { MenuItem, OutlinedInput, Select, SelectChangeEvent, ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import makeStyles from '@mui/styles/makeStyles';
+
+import { CmTextInput } from '@/components/atoms/CmDataInput';
+
+import * as CmStyle from '@/stylesheets/common';
+import { ReactComponent as SearchIcon } from '@/stylesheets/images/SearchIcon.svg';
+
 import { FilterTypes, SubmitActionTypes } from '../const';
 import { IFilterConfig, IFilterElementType } from '../types';
 import DropdownFilterInput from './DropdownFilterInput';
 import SimpleFilterInput from './SimpleFilterInput';
 
-import { CmButton } from '@/components/atoms/CmButton';
-import { ReactComponent as DeleteIcon } from '@/stylesheets/images/cmCardDelIcon.svg';
+const ITEM_HEIGHT = 28;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 8 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+function getStyles(name: string, personName: string, theme: any) {
+  return {
+    fontFamily: 'NotoSansCJKRegular',
+    fontSize: 13,
+    fontWeight:
+      personName.indexOf(name) === -1 ? theme.typography.fontWeightRegular : theme.typography.fontWeightMedium,
+  };
+}
+
+const names = ['Test01', 'Test02', 'Test03', 'Test04', 'Test05', 'Test06', 'Test07'];
+// type propsType = {
+//   className: string;
+// };
+// Styled
+const useStyles = makeStyles(() => ({
+  toggleBtn: {
+    '& button': {
+      padding: 0,
+      '& svg:not([class*="MuiSvgIcon"])': {
+        margin: 'auto 10px',
+      },
+      '& .MuiFormControl': {
+        margin: 0,
+      },
+      '& .MuiInputBase-root': {
+        padding: 0,
+        lineHeight: 2,
+        boxShadow: 'none',
+        '& .MuiOutlinedInput-notchedOutline': {
+          border: 0,
+        },
+      },
+    },
+    '& .MuiOutlinedInput-input': {
+      padding: '2.5px 35px 2.5px 14px !important',
+      fontSize: '13px',
+      fontFamily: CmStyle.notoSansDJKFont.regular,
+      background: '#fff',
+      '& em': {
+        fontStyle: 'normal',
+      },
+    },
+  },
+}));
 
 interface IFilterControlsProps {
   filterConfig: IFilterConfig;
@@ -62,15 +124,34 @@ const FilterControls = ({ filterConfig, onTriggerQuery }: IFilterControlsProps) 
     }
   };
 
+  const [alignment, setAlignment] = useState('left');
+
+  // const handleAlignment = (event, newAlignment) => {
+  const handleAlignment = () => {
+    // setAlignment(newAlignment);
+  };
+  const classes = useStyles();
+  // const { className } = props;
+  const theme = useTheme();
+  const [personName, setPersonName] = useState<any>([]);
+
+  const handleTgSelChange = (e: SelectChangeEvent<any>) => {
+    const {
+      target: { value },
+    } = e;
+    // setPersonName(typeof value === 'string' ? value.split(',') : value);
+    setPersonName(value);
+  };
+
   return (
     <>
-      <Stack
+      {/* <Stack
         direction="row"
         spacing={2}
       >
         <CmButton
           variant="contained"
-          startIcon={<DeleteIcon />}
+          startIcon={<SearchIcon />}
           btnTitle="Contained"
         />
         {filterConfig?.filters?.map((filterItem) => {
@@ -85,7 +166,49 @@ const FilterControls = ({ filterConfig, onTriggerQuery }: IFilterControlsProps) 
         >
           {filterConfig.submitLabel}
         </Button>
-      )}
+      )} */}
+      <ToggleButtonGroup
+        className={classes.toggleBtn}
+        value={alignment}
+        exclusive
+        onChange={handleAlignment}
+      >
+        <ToggleButton value="left">
+          <SearchIcon />
+        </ToggleButton>
+        <ToggleButton value="center">
+          <Select
+            // className={className}
+            displayEmpty
+            value={personName}
+            onChange={handleTgSelChange}
+            input={<OutlinedInput />}
+            IconComponent={ArrowDownIcon}
+            renderValue={(selected) => {
+              if (selected.length === 0) {
+                return <em>Select For Menu</em>;
+              }
+
+              return selected.join(', ');
+            }}
+            MenuProps={MenuProps}
+            inputProps={{ 'aria-label': 'Without label' }}
+          >
+            {names.map((name) => (
+              <MenuItem
+                key={name}
+                value={name}
+                style={getStyles(name, personName, theme)}
+              >
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </ToggleButton>
+        <ToggleButton value="right">
+          <CmTextInput />
+        </ToggleButton>
+      </ToggleButtonGroup>
     </>
   );
 };
