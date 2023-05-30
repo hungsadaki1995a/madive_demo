772d590234198ff33631dc620df4a0736dc12488
tmaxadmin <Admin@tmaxadmin>
2023-05-30 15:03:59 +0900

    CmSearch - Chip Style 보완

--- src/components/templates/CmSearch.tsx
+++ src/components/templates/CmSearch.tsx
@@ -9,10 +9,11 @@
  * 2023.05.19   김정아 차장   최초 작성
  ******************************************************/
 // import React, { useEffect, useState } from 'react';
-import { notoSansDJKFont } from '@/stylesheets/common';
 import SearchIcon from '@mui/icons-material/SearchOutlined';
 import { Button, Chip, Paper, Stack, TextField } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
+
+import { notoSansDJKFont } from '@/stylesheets/common';
 
 // Styled
 const useStyles = makeStyles(() => ({
@@ -42,6 +43,13 @@ const useStyles = makeStyles(() => ({
       height: '24px',
       '& .MuiChip-label': {
         lineHeight: 1,
+      },
+      '& .MuiSvgIcon-root': {
+        opacity: 0,
+        transition: 'opacity 250ms',
+      },
+      '&:hover .MuiSvgIcon-root': {
+        opacity: 1,
       },
     },
     // TextField
@@ -88,7 +96,7 @@ function CmSearch() {
         placeholder="검색할 항목을 입력해 주세요."
       />
       <Stack spacing={1}>
-        <Button variant="text">전체 삭제</Button>
+        <Button variant="text">검색조건 저장</Button>
         <Button variant="text">전체 삭제</Button>
       </Stack>
     </Paper>
