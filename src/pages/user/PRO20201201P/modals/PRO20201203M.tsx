d95ce2a2c59927cac5feea252b2a61499a136688
DESKTOP-0SUK8VN\gaury <Admin@tmaxadmin>
2023-06-03 17:26:41 +0900

    PRO20201202M, PRO20201203M publishing correction

--- src/pages/user/PRO20201201P/modals/PRO20201203M.tsx
+++ src/pages/user/PRO20201201P/modals/PRO20201203M.tsx
@@ -1,3 +1,10 @@
+// import { TextField } from '@mui/material';
+// import CmModal from '@/components/atoms/CmModal';
+// type EditUserModalProps = {
+//   visible: boolean;
+//   handleSave?: () => void;
+//   handleClose: () => void;
+// };
 import { forwardRef } from 'react';
 
 import { observer } from 'mobx-react';
@@ -35,3 +42,65 @@ const UpdateModal = ({ onSuccess }: UpdateModalProps, ref: React.Ref<UserFormRef
 };
 
 export default observer(forwardRef(UpdateModal));
+// export default function EditUserModal({ visible, handleSave, handleClose }: EditUserModalProps) {
+//   return (
+//     <CmModal
+//       title="Edit User"
+//       visible={visible}
+//       onSave={handleSave}
+//       onClose={handleClose}
+//       className="medium"
+//     >
+//       {/* contents */}
+// <label className="labelFormArea">
+//   <span>Vender</span>
+//   <TextField
+//     className="labelTextField"
+//     defaultValue="NQT"
+//     size="small"
+//   />
+// </label>
+// <label className="labelFormArea">
+//   <span>Alias</span>
+//   <TextField
+//     className="labelTextField"
+//     defaultValue="22112344"
+//     disabled
+//     size="small"
+//   />
+// </label>
+// <label className="labelFormArea">
+//   <span>ID</span>
+//   <TextField
+//     className="labelTextField"
+//     defaultValue="4185"
+//     size="small"
+//   />
+// </label>
+// <label className="labelFormArea">
+//   <span>Password</span>
+//   <TextField
+//     className="labelTextField"
+//     defaultValue="3185"
+//     size="small"
+//   />
+// </label>
+// <label className="labelFormArea">
+//   <span>IP</span>
+//   <TextField
+//     className="labelTextField"
+//     defaultValue="1111"
+//     size="small"
+//   />
+// </label>
+// <label className="labelFormArea">
+//   <span>Port</span>
+//   <TextField
+//     className="labelTextField"
+//     defaultValue="1111"
+//     size="small"
+//   />
+// </label>
+//     </CmModal>
+//   );
+// }
