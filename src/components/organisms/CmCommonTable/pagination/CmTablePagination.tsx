d0e8326da00fcbc6a0403431b9def5e3da9ecb9c
tmaxadmin <Admin@tmaxadmin>
2023-06-16 15:53:17 +0900

    Table Style Correction

--- src/components/organisms/CmCommonTable/pagination/CmTablePagination.tsx
+++ src/components/organisms/CmCommonTable/pagination/CmTablePagination.tsx
@@ -1,8 +1,32 @@
 import { TablePagination } from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
 
 import { rowsPerPageDefault, rowsPerPageOptionsDefault } from '@/utils/const/form.const';
 
 import { IPaginationConfig } from '../types';
+
+// Styled
+const useStyles = makeStyles(({ palette }) => ({
+  pagination: {
+    borderBottom: 0,
+    '& *': {
+      fontSize: '12px',
+      lineHeight: '16px',
+      color: palette.neutralLight[800],
+    },
+    '& div[class*="MuiTablePagination-select"] svg': {
+      width: '18px',
+      height: '18px',
+      marginTop: '-3px',
+      '& path': {
+        color: palette.neutralLight[500],
+      },
+    },
+    '& ~ label[class*="makeStyles-button"]': {
+      marginLeft: '8px',
+    },
+  },
+}));
 
 const CmTablePagination = ({
   currentPage,
@@ -19,9 +43,11 @@ const CmTablePagination = ({
   const handleRowsPerPageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     onRowsPerPageChange?.(Number(event.target.value));
   };
+  const classes = useStyles();
 
   return (
     <TablePagination
+      className={classes.pagination}
       rowsPerPageOptions={rowsPerPageOptions}
       rowsPerPage={rowsPerPage}
       count={totalCount}
--- src/components/organisms/CmCommonTable/tablePartitions/TableGrid.tsx
+++ src/components/organisms/CmCommonTable/tablePartitions/TableGrid.tsx
@@ -1,5 +1,8 @@
 import { styled, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 
+import FailIcon from '@/stylesheets/images/FailIcon.svg';
+import SuccessIcon from '@/stylesheets/images/SuccessIcon.svg';
+
 import { IPlainObject, TableLayoutProps } from '../types';
 import BodyCheckBoxCell from './BodyCheckBoxCell';
 import HeaderCheckBoxCell from './HeaderCheckBoxCell';
@@ -8,56 +11,52 @@ import TableHeaderCell from './TableHeaderCell';
 
 const TableHeadComponent = styled(TableHead)(({ theme }) => ({
   '& .MuiTableRow-head': {
-    backgroundColor: '#f4f7fc',
+    // backgroundColor: '#f4f7fc',
+    borderBottom: '2px solid',
+    borderColor: theme.palette.neutralLight[200],
     '& .MuiTableCell-head': {
-      color: '#444',
+      color: theme.palette.neutralLight[500],
       fontSize: '13px',
-      height: '34px',
-      padding: '0 15px',
+      height: '40px',
+      padding: '0 8px',
       fontWeight: theme.typography.fontWeightMedium,
-      borderRight: '1px solid #dbdfe1',
-      '&:last-child': {
-        borderRight: 'none',
-      },
+      // borderRight: '1px solid #dbdfe1',
+      // '&:last-child': {
+      //   borderRight: 'none',
+      // },
     },
   },
 }));
 const TableBodyComponent = styled(TableBody)(({ theme }) => ({
-  // '& .MuiTableRow-root': {
-  //   '&:nth-child(even)': {
-  //     backgroundColor: '#f9fafa',
-  //   },
-  //   '& .MuiTableCell-body': {
-  //     padding: '5px 15px',
-  //     fontSize: '12px',
-  //     color: '#888888',
-  //     // ellipsis
-  //     overflow: 'hidden',
-  //     whiteSpace: 'nowrap',
-  //     textOverflow: 'ellipsis',
-  //     wordBreak: 'break-all',
-  //     // Fail, Error
-  //     '&.error': {
-  //       color: '#D93E2E',
-  //       paddingLeft: '20px',
-  //       backgroundImage: `url(${FailIcon}) left center no-repeat`,
-  //     },
-  //     // Success
-  //     '&.success': {
-  //       color: '#30BE8B',
-  //       paddingLeft: '20px',
-  //       backgroundImage: `url(${SuccessIcon}) left center no-repeat`,
-  //     },
-  //   },
-  //   '&:hover': {
-  //     '& .MuiTableCell-body': {
-  //       backgroundColor: '#e6f4ff',
-  //       color: '#30404d',
-  //       fontSize: '13px',
-  //       fontWeight: theme.typography.fontWeightMedium,
-  //     },
-  //   },
-  // },
+  '& .MuiTableRow-root': {
+    '&:nth-child(even)': {
+      backgroundColor: '#f9fafa',
+    },
+    '& .MuiTableCell-body': {
+      padding: '0 8px',
+      fontSize: '12px',
+      height: '32px',
+      color: theme.palette.neutralLight[800],
+      fontWeight: theme.typography.fontWeightRegular,
+      // ellipsis
+      overflow: 'hidden',
+      whiteSpace: 'nowrap',
+      textOverflow: 'ellipsis',
+      wordBreak: 'break-all',
+      // Fail, Error
+      '&.error': {
+        color: '#D93E2E',
+        paddingLeft: '20px',
+        backgroundImage: `url(${FailIcon}) left center no-repeat`,
+      },
+      // Success
+      '&.success': {
+        color: '#30BE8B',
+        paddingLeft: '20px',
+        backgroundImage: `url(${SuccessIcon}) left center no-repeat`,
+      },
+    },
+  },
 }));
 
 const StyledTableRow = styled(TableRow)(({ hidden = false }: { hidden: boolean }) => ({
